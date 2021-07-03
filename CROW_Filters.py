822801c8e7f7d7ebd425cae414807e7975b62743
Trevor Steiner <stei1133@umn.edu>
2021-07-03 11:42:05 -0700

    full conversion from matlab to python

--- CROW_Filters.py
+++ CROW_Filters.py
@@ -6,7 +6,7 @@ from scipy.io import loadmat
 import pandas as pd
 import matplotlib.pyplot as plt
 
-lambda0 = np.linspace(1200,1800,200)
+lambda0 = np.linspace(1200,1800,200)*1e-9
 height_wg=0.4e-6
 w_rr=0.69e-6
 n_wg= 3.2658
@@ -27,19 +27,19 @@ data=np.asarray(xtest['ans'])
 f=np.sort([data[i][0] for i in range(len(data))])
 neff=np.sort([data[i][1] for i in range(len(data))])
 
-ang_neff=interpolate.interp1d(f,neff,kind='linear')
+ang_neff=interpolate.interp1d(f,neff,kind='cubic')
 fnew=np.linspace(f[0],f[-1],len(lambda0))
-plt.plot(fnew,ang_neff(fnew))
-plt.show()
-# L_rt=2*pi*radius
-# beta=ang_neff * 2*pi./lambda
-# gamma=0 #lossless coupling
-# for i=1:4
-#     C(i)=sqrt(((1-kappaList(i))*(1-gamma)))
-#     S(i)=1/1j*sqrt(((1-gamma*1j)*kappaList(i)))
-# end
-# xi=exp(alpha*L_rt/2)*exp(-1j*beta*L_rt)
-# T_drop=S(1)*S(2)*S(3)*S(4)*xi./(1-C(1)*C(2)*xi.^2+C(1)*C(3)*xi.^2+C(2)*C(4)*xi.^3+C(1)*C(2)*C(3)*C(4)*xi.^2)
-# T_drop_dB=10*log10(T_drop)
+L_rt=2*pi*radius
+beta=ang_neff * 2*pi/lambda0
+gamma=0 #lossless coupling
+C=[]
+S=[]
+for i in range(0,4):
+     C.append(math.sqrt(((1-kappaList[i])*(1-gamma))))
+     S.append(1/1j*math.sqrt(((1-gamma*1j)*kappaList[i])))
+xi=math.exp(alpha*L_rt/2)*exp(-1j*beta*L_rt)
+T_drop=S(1)*S(2)*S(3)*S(4)*xi./(1-C(1)*C(2)*xi.^2+C(1)*C(3)*xi.^2+C(2)*C(4)*xi.^3+C(1)*C(2)*C(3)*C(4)*xi.^2)
+T_drop_dB=10*math.log10(T_drop)
 
-# plot(lambda,T_drop_dB)
+plt.plot(lambda0,T_drop_dB)
+plt.show()
