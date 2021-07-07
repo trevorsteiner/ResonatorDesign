15fd0f54d6cd869d3d445a8a3fb83de7b241967f
Trevor Steiner <stei1133@umn.edu>
2021-07-07 15:22:40 -0700

    Lumerical compatibility with import option for previous .mat files with f, neff

--- CROW_Filters.py
+++ CROW_Filters.py
@@ -3,104 +3,117 @@ from scipy import interpolate
 from scipy.constants import pi, c
 import math
 import cmath
-from scipy.io import loadmat
+from scipy.io import loadmat, savemat
 import pandas as pd
 import matplotlib.pyplot as plt
 
-lambda0 = np.linspace(1200,1800,200)*1e-9
+lambda0 = np.linspace(1540,1560,200)*1e-9
 height_wg=0.4e-6
 w_wg=0.69e-6
-radius = 600*1e-6
+radius = 5*1e-6
 alpha_dB_cm = 0.40
 kappaList=np.array([0.5,0.14,0.14,0.5])
+loaddata=1 #boolean specifying if a data file can be loaded
 
 ## CONVERSION ##############################################################
 alpha = alpha_dB_cm*10*math.log(10)                  # per m
 
+if loaddata:
+    from tkinter import Tk     # from tkinter import Tk for Python 3.x
+    from tkinter.filedialog import askopenfilename
 
-## LUMERICAL Automation for effective index
-import imp
-lumapi = imp.load_source("lumapi.py", "C:\\Program Files\\Lumerical\\v211\\api\python\\lumapi.py") # Windows
-mode=lumapi.MODE(hide=False)
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 
-## Add AlGaAs material with Al concentration x to Lumerical
-from FindIndex import FindIndex
-x=0.2
-wgMat='AlGaAs'
-material=mode.addmaterial('Sampled 3D Data')
-mode.setmaterial(material,"Name",wgMat)
-indexlist=FindIndex(x,lambda0)
-indexdata=[[c/lambda0[i],np.power(indexlist[i],2)] for i in range(len(lambda0))]
-mode.setmaterial(wgMat,"sampled data",np.asarray(indexdata))
+    ## EFFECTIVE INDEX CALCULATIONS ##################################
+    #Eventually plan to replace this section with some Lumerical scripting
+    data=loadmat(filename)
+    f=data['f']
+    neff=data['neff']
+    f=[f[i] for i in range(len(f))][0]
+    neff=[neff[i] for i in range(len(neff))][0]
+    #f=np.sort([data[i][0] for i in range(len(data))])
+    #neff=np.sort([data[i][1] for i in range(len(data))])
+else:
+    ## LUMERICAL Automation for effective index
+    import imp
+    lumapi = imp.load_source("lumapi.py", "C:\\Program Files\\Lumerical\\v211\\api\python\\lumapi.py") # Windows
+    mode=lumapi.MODE(hide=False)
 
-mode.switchtolayout()
-mode.deleteall()
-mode.addrect()
-mode.set('name','Waveguide')
-mode.set('x span',w_wg)
-mode.set('y span',height_wg)
-mode.set('x',0)
-mode.set('y',0)
-mode.set('material',wgMat)
-    
-mode.addrect()
-mode.set('name','Oxide')        
-mode.set('x span',w_wg+40e-6)
-mode.set('x',0)
-mode.set('y',0)
-mode.set('y span',height_wg+6e-6)
-mode.set('material','SiO2 (Glass) - Palik')
-mode.set('override mesh order from material database',1)
-mode.set('mesh order',3)
-mode.set('alpha',0.2)
+    ## Add AlGaAs material with Al concentration x to Lumerical
+    from FindIndex import FindIndex
+    x=0.2
+    wgMat='AlGaAs'
+    material=mode.addmaterial('Sampled 3D Data')
+    mode.setmaterial(material,"Name",wgMat)
+    indexlist=FindIndex(x,lambda0)
+    indexdata=[[c/lambda0[i],np.power(indexlist[i],2)] for i in range(len(lambda0))]
+    mode.setmaterial(wgMat,"sampled data",np.asarray(indexdata))
 
-mode.addmesh()  # mesh override, higher resolution in the waveguide.
-mode.set('x span',w_wg+0.1e-6)
-mode.set('y span',height_wg+0.1e-6)
-mode.set('x',0)
-mode.set('y',0)
-mode.set('dx',10e-9)
-mode.set('dy',10e-9)
-    
-mode.addfde() #  create simulate mesh
-mode.set('x span',w_wg+10e-6)
-mode.set('y span',height_wg+6e-6)
-mode.set('x',0)
-mode.set('y',0)
-mode.set('mesh cells x',100) 
-mode.set('mesh cells y',200)
-mode.set('x min bc','PML')
-mode.set('y min bc','PML')
-mode.set('x max bc','PML')
-mode.set('y max bc','PML')
+    mode.switchtolayout()
+    mode.deleteall()
+    mode.addrect()
+    mode.set('name','Waveguide')
+    mode.set('x span',w_wg)
+    mode.set('y span',height_wg)
+    mode.set('x',0)
+    mode.set('y',0)
+    mode.set('material',wgMat)
+        
+    mode.addrect()
+    mode.set('name','Oxide')        
+    mode.set('x span',w_wg+40e-6)
+    mode.set('x',0)
+    mode.set('y',0)
+    mode.set('y span',height_wg+6e-6)
+    mode.set('material','SiO2 (Glass) - Palik')
+    mode.set('override mesh order from material database',1)
+    mode.set('mesh order',3)
+    mode.set('alpha',0.2)
 
-mode.run()
-mode.setanalysis('wavelength',lambda0[0])
-mode.findmodes()
-mode.selectmode(1)
-mode.setanalysis("track selected mode",1)
-mode.setanalysis('stop wavelength',lambda0[-1])
-mode.setanalysis('number of points',10)
-mode.setanalysis('number of test modes',4)
-mode.frequencysweep()
-neff=mode.getdata('frequencysweep','neff')
-f=mode.getdata('frequencysweep','f')
+    mode.addmesh()  # mesh override, higher resolution in the waveguide.
+    mode.set('x span',w_wg+0.1e-6)
+    mode.set('y span',height_wg+0.1e-6)
+    mode.set('x',0)
+    mode.set('y',0)
+    mode.set('dx',10e-9)
+    mode.set('dy',10e-9)
+        
+    mode.addfde() #  create simulate mesh
+    mode.set('x span',w_wg+10e-6)
+    mode.set('y span',height_wg+6e-6)
+    mode.set('x',0)
+    mode.set('y',0)
+    mode.set('mesh cells x',100) 
+    mode.set('mesh cells y',200)
+    mode.set('x min bc','PML')
+    mode.set('y min bc','PML')
+    mode.set('x max bc','PML')
+    mode.set('y max bc','PML')
 
-# ## EFFECTIVE INDEX CALCULATIONS ##################################
-# #Eventually plan to replace this section with some Lumerical scripting
-# xtest=loadmat('480nmbend_neff.mat')
-# data=np.asarray(xtest['ans'])
+    mode.run()
+    mode.setanalysis('wavelength',lambda0[0])
+    mode.findmodes()
+    mode.selectmode(1)
+    mode.setanalysis("track selected mode",1)
+    mode.setanalysis('stop wavelength',lambda0[-1])
+    mode.setanalysis('number of points',10)
+    mode.setanalysis('number of test modes',4)
+    mode.frequencysweep()
+    neff=mode.getdata('frequencysweep','neff')
+    f=mode.getdata('frequencysweep','f')
+    f=[f[i][0] for i in range(len(f))]
+    neff=[neff[i][0] for i in range(len(neff))]
+    mdic={"f":f,"neff":neff}
+    savemat('neff_f_{}_{}.mat'.format(height_wg*1e9,w_wg*1e9),mdic)
 
-# f=np.sort([data[i][0] for i in range(len(data))])
-# neff=np.sort([data[i][1] for i in range(len(data))])
 
 ## Increased sampling of neff
-ang_neff=interpolate.interp1d(f,neff,kind='cubic',axis=1)
-fnew=np.linspace(f[0],f[-1],len(lambda0))
+ang_neff=interpolate.interp1d(f,neff,kind='cubic')
 
 ## Round trip distance, propagation constant
 L_rt=2*pi*radius
-beta=ang_neff(fnew) * 2*pi/lambda0
+beta=ang_neff(c/lambda0) * 2*pi/lambda0
 gamma=0 #lossless coupling
 
 ## Transfer matrix elements
@@ -118,5 +131,7 @@ T_drop=S[0]*S[1]*S[2]*S[3]*xi/(1-C[0]*C[1]*np.power(xi,2)+C[0]*C[2]*np.power(xi,
 T_drop_dB=10*np.log10(T_drop)
 
 #Plot
-plt.plot(lambda0,T_drop_dB)
+plt.plot(lambda0*1e9,T_drop_dB)
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('T_drop (dB)')
 plt.show()
